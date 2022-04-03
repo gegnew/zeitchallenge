3b4a867e882c72df735afd26224007545df8b119
gegnew <g.egnew@gmail.com>
2022-04-03 17:54:23 -0600

    deps: update requirements

--- backend/setup.py
+++ backend/setup.py
@@ -1,19 +1,21 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="./",
+    name="zeitchallenge",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.1.0",
     description="Sr SWE Take Home Challenge",
     author="Gerrit Egnew",
     author_email="g.egnew@gmail.com",
     license="",
     install_requires=[
-        "fastapi~=0.75"
-        "uvicorn~=0.17"
-        "requests~=2.27"
-        "python-multipart~=0.0"
-        "jinja2-time~=0.2"
+        "fastapi~=0.75",
+        "uvicorn~=0.17",
+        "requests~=2.27",
+        "python-multipart~=0.0",
+        "jinja2-time~=0.2",
+        "python-jose~=3.3",
+        "python-dotenv~=0.20"
     ],
     tests_require=["pytest~=7.1"],
     python_requires=">=3.7",
